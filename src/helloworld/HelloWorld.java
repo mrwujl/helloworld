69f303f3b98238369162de9205b5106c9dfc2300
mrwujl <291329275@qq.com>
2018-02-23 15:04:26 +0800

    test

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+/**
+ * @Classname HelloWorld.helloworld.HelloWorld
+ * @Description：
+ * @Author 吴建林  wjl
+ * @Date 2018年2月23日  下午3:02:18
+ * @Version 1.0
+ * @Remark create by generator
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
